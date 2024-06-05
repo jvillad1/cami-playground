a0cd64ae78e005cd284b767e8b1c0a8f679ed830
jvillada <juan.villada@mercadolibre.com.co>
2024-06-05 14:03:52 -0500

    [feat]: Add FirstActivity.kt

--- app/src/main/java/com/cami/playground/FirstActivity.kt
+++ app/src/main/java/com/cami/playground/FirstActivity.kt
@@ -1,34 +1,22 @@
 package com.cami.playground
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.cami.playground.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_first)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.buttonFirst.setOnClickListener {
-            Intent(this, FirstActivity::class.java).also {
-                startActivity(it)
-            }
         }
     }
 }
