b4f719034fb9479a399253d12c5fee8a436d0c5b
jvillada <juan.villada@mercadolibre.com.co>
2024-06-05 14:10:11 -0500

    [feat]: Add SecondActivity.kt

--- app/src/main/java/com/cami/playground/SecondActivity.kt
+++ app/src/main/java/com/cami/playground/SecondActivity.kt
@@ -1,34 +1,26 @@
 package com.cami.playground
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.cami.playground.databinding.ActivityFirstBinding
+import com.cami.playground.databinding.ActivitySecondBinding
 
-class FirstActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityFirstBinding
-
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityFirstBinding.inflate(layoutInflater)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.buttonSecond.setOnClickListener {
-            Intent(this, SecondActivity::class.java).also {
-                startActivity(it)
-            }
         }
     }
 }
